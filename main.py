53d8b4ac6508a6187be0310c059194872099d694
Aunim HASSAN <aunim.hassan@gmail.com>
2025-09-03 09:09:33 +0200

    feat: cipher caesar

---
+++ main.py
@@ -0,0 +1,21 @@
+def caesar(text:str, offset:int = 1) -> str:
+    result = ""
+    for char in text:
+        # Lowercase
+        if "a" <= char <= "z":
+            index = ord(char) - ord("a")
+            index = (index + offset) % 26
+            result += chr(index + ord("a"))
+            continue
+
+        # Uppercase
+        if "A" <= char <= "Z":
+            index = ord(char) - ord("A")
+            index = (index + offset) % 26
+            result += chr(index + ord("A"))
+            continue
+
+        # Others
+        result += char
+    
+    return result
